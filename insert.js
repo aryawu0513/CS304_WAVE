539efc93cafb5e814c0c8c50bbe7770b3dcd4101
mjdgv <md103@wellesley.edu>
2024-04-19 11:02:35 -0400

    commented out dataset reset

--- insert.js
+++ insert.js
@@ -65,7 +65,7 @@ async function main() {
     hosting: [2]});
     await add(db, 'users', {name: "Bella Steedly", username: 'bella', hash:'bella', wellesleyEmail: 'bs102@wellesley.edu', friends: [1,2,4], 
     rsvp:[2,3], hosting: [3]});
-    await add(db, 'users', {name: "Austen Boodell", wellesleyEmail: 'eb115@wellesley.edu', friends: [1,2,3], rsvp:[3], 
+    await add(db, 'users', {name: "Austen Boodell", username: 'austen', hash: 'austen', wellesleyEmail: 'eb115@wellesley.edu', friends: [1,2,3], rsvp:[3], 
     hosting: []});
 
     //await Connection.open(mongoUri, myDBName);
